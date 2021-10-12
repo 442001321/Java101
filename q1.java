cb3583b832d304c7b091c56da511545ce9c9e1a2
حسين سعود العبدلي <91111031+442001321@users.noreply.github.com>
2021-10-12 09:18:50 +0300

    Create q1.java

---
+++ q1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class q1.java{
+public static void q1.java(String[] args) {
+    Scanner scan = new Scanner (System.in);
+    System.out.println("h");
+    int numScanner.nextInt();
+    if (num%2==0);{
+    if else (num >0);
+    System.out.println("evan");
+    else (num <0);
+    System.out.println("odd");
+ }
+}
+}
