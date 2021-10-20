430a11148828e5125f3a3744c9927da1421d2db5
حسين سعود العبدلي <91111031+442001321@users.noreply.github.com>
2021-10-20 21:00:32 +0300

    Create mid.java

---
+++ mid.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Mid.java{
+public static void mid.java(String[] args) {
+Scanner scan = new Scanner(System.in);
+String neme=scan.nextLine();
+int age=scan.nextInt();
+ System.out.println(2021-age);
+    if (age<=40){
+     System.out.println("user name");
+     System.out.println("user age");
+     else if 
+     (age>=30&&age<=39);
+     System.out.println("user name");
+     System.out.println("user age");
+     else
+     System.out.println("user name");
+     System.out.println("user age");
+     }
+ 
+}
+}
