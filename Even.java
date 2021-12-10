734fcc3a5bec1d44659739e58475e4c91075b4b5
حسين سعود العبدلي <91111031+442001321@users.noreply.github.com>
2021-12-10 20:13:08 +0300

    Create Even.java

---
+++ Even.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Even.java{
+    public static void main(String[] args) {
+     Scanner scan = new Scanner(System.in);
+     System.out.println("Enter the number");
+     int num1 = scan.nextInt();
+     if(num1 %2==0)
+      
+
+     System.out.println("Evan");
+     else
+     System.out.println("odd");
+    }}
+
