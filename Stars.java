ee0cdd698e70e9e6d8178e83e070c9cc5feb2a17
حسين سعود العبدلي <91111031+442001321@users.noreply.github.com>
2021-12-12 10:20:53 +0300

    Create Stars.java

---
+++ Stars.java
@@ -0,0 +1,14 @@
+public class Stars.java{
+	public static void main(String[] args) {
+		int x =5;
+		while(x>=1){
+		    int y=1;
+		    while(y<=x){
+		    System.out.print("*");
+		    ++y;
+		}
+		System.out.println();
+		--x;
+	}
+}
+}
