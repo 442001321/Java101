db1f22807a741a71b549a15901888736f294757c
حسين سعود العبدلي <91111031+442001321@users.noreply.github.com>
2021-12-12 23:12:03 +0300

    Create age.java

---
+++ age.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class age.java{
+public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    float x;
+    System.out.println("Enter the num");
+    x=scan.nextFloat();
+    System.out.println(1443-x);
+}
+}
