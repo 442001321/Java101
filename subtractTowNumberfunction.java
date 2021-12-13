bbf8266f647b6d8f22a38d23c0d3828f9e463c9e
حسين سعود العبدلي <91111031+442001321@users.noreply.github.com>
2021-12-13 21:46:56 +0300

    Create subtractTowNumberfunction.java

---
+++ subtractTowNumberfunction.java
@@ -0,0 +1,9 @@
+public class subtractTowNumberfunction.java {
+  static float mymethod(float num1 , float num2 ) {
+    return num1 - num2;
+  }
+  public static void main(String[] args) {
+    float x = mymethod(245, 76);
+    System.out.println(x);
+  }
+}
