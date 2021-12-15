c0ce5c8200376215f4c8df3a28d0ff497a4222bb
حسين سعود العبدلي <91111031+442001321@users.noreply.github.com>
2021-12-15 14:20:39 +0300

    Create readAndReplace.java

---
+++ readAndReplace.java
@@ -0,0 +1,13 @@
+public class readAndReplace.java{
+public static void main(String[] args){
+    String name="shift enter";
+    for(int h=0;h<=8;++h){
+        if(name.charAt(h)=='t'){
+            System.out.print('h');}
+            else{
+                System.out.print(name.charAt(h));
+            }
+        }
+    }
+    
+}
